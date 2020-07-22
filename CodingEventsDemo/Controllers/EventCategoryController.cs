cf005d613d3fb8c7edc5042b1783dc91e018a33b
rhagen87 <reynold.hagen@gmail.com>
2020-07-22 16:25:04 -0500

    19.4.5

---
+++ CodingEventsDemo/Controllers/EventCategoryController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CodingEventsDemo.Data;
+using CodingEventsDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodingEventsDemo.Controllers
+{
+    public class EventCategoryController : Controller
+    {
+        private EventDbContext context;
+
+        [HttpGet]
+        [Route("EventCategory/Index)"]
+        public IActionResult Index()
+        {
+            List<EventCategory> categories = context.Category.ToList();
+            return View(categories);
+        }
+    }
+}
--- CodingEventsDemo/Migrations/20200722211539_CategoryMigration.cs
+++ CodingEventsDemo/Migrations/20200722211539_CategoryMigration.cs
@@ -3,31 +3,28 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace CodingEventsDemo.Migrations
 {
-    public partial class InitialMigration : Migration
+    public partial class CategoryMigration : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.CreateTable(
-                name: "Events",
+                name: "Category",
                 columns: table => new
                 {
                     Id = table.Column<int>(nullable: false)
                         .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
-                    Name = table.Column<string>(nullable: true),
-                    Description = table.Column<string>(nullable: true),
-                    ContactEmail = table.Column<string>(nullable: true),
-                    Type = table.Column<int>(nullable: false)
+                    Name = table.Column<string>(nullable: true)
                 },
                 constraints: table =>
                 {
-                    table.PrimaryKey("PK_Events", x => x.Id);
+                    table.PrimaryKey("PK_Category", x => x.Id);
                 });
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropTable(
-                name: "Events");
+                name: "Category");
         }
     }
 }
